9b095f647c6cdcfcc8ff2bd8fe32ce49bfdc8834
Chekoo <chekochn@gmail.com>
2019-04-21 00:46:34 +0800

    day2

--- Task1/task1.py
+++ Task1/task1.py
@@ -1,7 +1,6 @@
-#coding: utf-8
 
 import socket
-from urllib.parse import urlparse
+import ssl
 """
 2017/02/16
 作业 1
@@ -20,130 +19,175 @@ send 函数的参数和 recv 函数的返回值都是 bytes 类型
 # 1
 # 补全函数
 def protocol_of_url(url):
-    '''
-    url 是字符串, 可能的值如下
-    'g.cn'
-    'g.cn/'
-    'g.cn:3000'
-    'g.cn:3000/search'
-    'http://g.cn'
-    'https://g.cn'
-    'http://g.cn/'
-
-    返回代表协议的字符串, 'http' 或者 'https'
-    '''
-    result = urlparse(url)
-    return result.scheme
-
+    pass
 
 # 2
 # 补全函数
 def host_of_url(url):
-    '''
-    url 是字符串, 可能的值如下
-    'g.cn'
-    'g.cn/'
-    'g.cn:3000'
-    'g.cn:3000/search'
-    'http://g.cn'
-    'https://g.cn'
-    'http://g.cn/'
+    pass
 
-    返回代表主机的字符串, 比如 'g.cn'
-    '''
-    result = urlparse(url)
-    return result.netloc
 
 
 # 3
 # 补全函数
 def port_of_url(url):
-    '''
-    url 是字符串, 可能的值如下
-    'g.cn'
-    'g.cn/'
-    'g.cn:3000'
-    'g.cn:3000/search'
-    'http://g.cn'
-    'https://g.cn'
-    'http://g.cn/'
+    pass
 
-    返回代表端口的字符串, 比如 '80' 或者 '3000'
-    注意, 如上课资料所述, 80 是默认端口
-    '''
-    result = urlparse(url)
-    return result.port
 # 4
 # 补全函数
 def path_of_url(url):
-    '''
-    url 是字符串, 可能的值如下
-    'g.cn'
-    'g.cn/'
-    'g.cn:3000'
-    'g.cn:3000/search'
-    'http://g.cn'
-    'https://g.cn'
-    'http://g.cn/'
+    pass
 
-    返回代表路径的字符串, 比如 '/' 或者 '/search'
-    注意, 如上课资料所述, 当没有给出路径的时候, 默认路径是 '/'
-    '''
-    result = urlparse(url)
-    return result.path
 
 
 # 4
 # 补全函数
 def parsed_url(url):
-    '''
-    url 是字符串, 可能的值如下
-    'g.cn'
-    'g.cn/'
-    'g.cn:3000'
-    'g.cn:3000/search'
-    'http://g.cn'
-    'https://g.cn'
-    'http://g.cn/'
-    返回一个 tuple, 内容如下 (protocol, host, port, path)
-    '''
-    result = urlparse(url)
-    tuple = (result.scheme, result.netloc, result.port, result.path)
 
+    # 檢查協議
+    protocol = 'http'
+    if url[:7] == 'http://':
+        u = url.split('://')[1]
+    elif url[:8] == 'https://':
+        u = url.split('://')[1]
+    else:
+        u = url
+
+    # 檢查默认 path
+    i = u.find('/')
+    if i == -1:  # find找不到返回-1
+        host = u
+        path = '/'
+    else:  # g.cn/hello
+        host = u[:1]
+        path = u[i:]
+
+    # 检查端口
+    port_dict = {
+        'http': 80,
+        'https': 443,
+    }
+
+    # 默认端口
+    port = port_dict[protocol]
+    if ':' in host:  # 若host里面有:
+        h = host.split(':')
+        host = h[0]
+        port = int(h[1])
+
+    return protocol, host, port, path
 
 # 5
 # 把向服务器发送 HTTP 请求并且获得数据这个过程封装成函数
 # 定义如下
+def parsed_response(r):
+    header, body = r.split('\r\n\r\n', 1)
+    h = header.split('\r\n')
+    status_code = h[0].split()[1]
+    status_code = int(status_code)
+
+
+def socket_by_protocol(protocol):
+    '''根据一个协议返回一个socket实例'''
+    if protocol == 'http':
+        s = socket.socket()
+    else:
+        s = ssl.wrap_socket(socket.socket())
+    return s
+
+
+def response_by_socket(s):
+    '''
+    参数是一个socket实例
+    返回这个socket读取的所有数据
+    '''
+    response = b''
+    buffer_size = 1024
+    while True:
+        r = s.recv(buffer_size)
+        if len(r) == 0:
+            break
+        response += r
+    return response
+
+
 def get(url):
     '''
-    本函数使用上课代码 client.py 中的方式使用 socket 连接服务器
-    获取服务器返回的数据并返回
-    注意, 返回的数据类型为 bytes
+    用GET请求url并返回响应
     '''
-    result = urlparse(url)
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    host = result.netloc
-    port = result.port
+    protocol, host, port, path = parsed_url(url)
+
+    s = socket_by_protocol(protocol)
     s.connect((host, port))
-    ip, port = s.getsockname()
-    print('本機ip和port {} {}'.format(ip, port))
-    http_request = 'GET / HTTP/1.1\r\nhost:{}\r\n\r\n'.format(host)
-    request = http_request.encode('utf-8')
-    print('請求', request)
-    s.send(request)
-    buffer_size = 1000;
-    a = b''
-    while True:
-        response = s.recv(buffer_size)
-        a += response
-        if len(response) < buffer_size:
-            break
+
+    request = 'GET {} HTTP/1.1\r\nhost: {}\r\nConnection: close\r\n\r\n'.format(path, host)
+    encoding = 'utf-8'
+    s.send(request.encode(encoding))
+
+    response = response_by_socket(s)
+    r = response.decode(encoding)
+
+    status_code, headers, body = parsed_response(r)
+    if status_code == 301:
+        url = headers['Location']
+        return get(url)
+
+    return status_code, headers, body
+
+
 # 使用
 def main():
     url = 'http://movie.douban.com/top250'
-    r = get(url)
-    print(r)
+    status_code, headers, body = get(url)
+    print(status_code, headers, body)
 
+def test_parsed_url():
+    http = 'http'
+    https = 'https'
+    host = 'g.cn'
+    path = '/'
+    test_items = [
+        ('http://g.cn', (http, host, 80, path)),
+        ('http://g.cn', (http, host, 80, path)),
+        ('http://g.cn:90', (http, host, 90, path)),
+        ('http://g.cn:90/', (http, host, 90, path)),
+        ('https://g.cn', (https, host, 443, path)),
+        ('https://g.cn:233/', (https, host, 233, path)),
+    ]
+    for t in test_items:
+        url, expected = t
+        u = parsed_url(url)
+        # assert是一个语句，叫做断言，若条件成立，则通过测试
+        e = "parsed_url ERROR, ({}) ({}) ({})".format(url, u, expected)
+        assert u == expected, e
+
+
+def test_parsed_response():
+    response = 'HTTP/1.1 301 Moved Permanently\r\n' \
+        'Content-Type: text/html\r\n' \
+        'Location: https://movie.douban.com/top250\r\n' \
+        'Content-Length: 178\r\n\r\n' \
+        'test body'
+    status_code, header, body = parsed_response(response)
+    assert status_code == 301
+    assert len(list(header.keys())) == 3
+    assert body == 'test body'
+
+
+def test_get():
+    urls = [
+        'http://movie.douban.com/top250',
+        'https://movie.douban.com/top250',
+    ]
+    for u in urls:
+        get(u)
+
+
+def test():
+   test_parsed_url()
+   test_get()
+   test_parsed_response()
 
 if __name__ == '__main__':
+    test()
     main()
